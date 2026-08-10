af9496f5818f25811401c0342986e8b5f07a52b0
Anwar Fauzi <anwar.fauzy18@gmail.com>
2026-08-10 10:01:06 +0700

    seeder sekolah

--- database/seeders/SekolahSeeder.php
+++ database/seeders/SekolahSeeder.php
@@ -14,15 +14,16 @@ class SekolahSeeder extends Seeder
     public function run(): void
     {
         Sekolah::create([
-            'nama_sekolah' => 'TK Islam Prima Insani',
+            'nama_sekolah' => 'TK Islam Plus Prima Insani',
             'npsn' => '69956855',
-            'kode_pos' => '44112',
-            'nomor_telpon' => '(0262) 231348',
             'alamat' => 'Jl. Ciledug No. 281',
-            'website' => 'https://www.primainsani.sch.id',
-            'email' => 'gis.primainsani.sch.id',
-            'kepala_sekolah' => 'Santi Rismayanti, M.Pd.',
-            'nip_kepala_sekolah' => '19023L1040211212119024'
+            'kode' => '44112',
+            'telepon' => '081234567890',
+            'desa' => 'Kota Kulon',
+            'kecamatan' => 'Garut Kota',
+            'kabupaten' => 'Garut',
+            'provinsi' => 'Jawa Barat',
+            'logo' => null,
         ]);
     }
 }
