9b096becaf98d98a78b30e622906943402846da2
__engling. <azura.ayl22@gmail.com>
2026-09-07 08:57:01 +0700

    perbaikan footer

--- resources/views/kehadirans/index.blade.php
+++ resources/views/kehadirans/index.blade.php
@@ -80,20 +80,7 @@
 </div>
 @stop
 
-@section('footer')
-    <div class="row align-items-center">
-        <div class="col-12 col-md-6 text-center text-md-left mb-2 mb-md-0">
-            <strong>
-                Copyright &copy; {{ date('Y') }} |
-                <a href="#">Yayasan Prima Insani</a>.
-            </strong>
-        </div>
-
-        <div class="col-12 col-md-6 text-center text-md-right">
-            <b>E-Raport</b> 
-        </div>
-    </div>
-@stop
+@include('layouts.footer')
 
 @section('css')
 <style>
--- resources/views/pengumumans/index.blade.php
+++ resources/views/pengumumans/index.blade.php
@@ -85,19 +85,7 @@
 
 @stop
 
-@section('footer')
-    <div class="row align-items-center">
-        <div class="col-12 col-md-6 text-center text-md-left mb-2 mb-md-0">
-            <strong>
-                Copyright &copy; {{ date('Y') }} |
-                <a href="#">Yayasan Prima Insani</a>.
-            </strong>
-        </div>
-        <div class="col-12 col-md-6 text-center text-md-right">
-            <b>E-Raport</b> 
-        </div>
-    </div>
-@stop
+@include('layouts.footer')
 
 @section('css')
     {{-- Add here extra stylesheets --}}
