6a93b38962186c6f3e51ef2ce4e998551fb50305
__engling. <azura.ayl22@gmail.com>
2026-09-22 10:00:56 +0700

    membenarkan url

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,6 @@ use App\Http\Controllers\NilaiKarakterController;
 use App\Http\Controllers\KesehatanTelingaController;
 use App\Http\Controllers\PdfController;
 use App\Http\Controllers\CatatanController;
-use Barryvdh\DomPDF\Facade\Pdf;
 
 Route::get('/', function () {
     return view('auth.login');
@@ -38,68 +37,60 @@ Auth::routes();
 
 Route::prefix('admin')->middleware(['auth'])->group(function () {
 
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::middleware(['role:admin'])->group(function () {
+        Route::resource('pengumuman', PengumumanController::class)->except(['show'])->whereNumber('pengumuman');
+        Route::resource('sekolah', SekolahController::class)->except(['create','show','edit','destroy'])->whereNumber('sekolah');
+        Route::resource('tahun_ajaran', TahunAjaranController::class);
+        
+        Route::resource('guru', GuruController::class);
+        Route::get('/guru/{id}/edit-password', [GuruController::class, 'editPassword'])->name('guru.edit-password');
+        Route::put('/guru/{id}/update-password', [GuruController::class, 'updatePassword'])->name('guru.update-password');
+        Route::put('/guru/{id}/update-user', [GuruController::class, 'updateUser'])->name('guru.update-user');
+
+        Route::post('/kelas/duplicate', [KelasController::class, 'duplicateFromPreviousSemester'])->name('kelas.duplicate');
+        Route::resource('kelas', KelasController::class)->parameters(['kelas' => 'kelas']);
+        Route::resource('siswa', SiswaController::class)->except(['show'])->whereNumber('siswa');
+
+        Route::resource('kriteria', KriteriaPenilaianController::class)->parameters(['kriteria' => 'kriteriapenilaian']);
+        Route::resource('karakter', KarakterController::class)->except(['show']);
+        Route::resource('capaian-perkembangan', CapaianPerkembanganController::class);
+        Route::resource('indikator', IndikatorController::class);
+    });
 
     Route::middleware(['role:admin|guru'])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::resource('anggota-kelas', AnggotaKelasController::class)
-    ->parameters([
-        'anggota-kela' => 'anggota kelas'
-    ]);
-    Route::resource('pdf', PdfController::class)->only(['show', 'index']);
-    });
-    
-    Route::middleware(['role:admin'])->group(function () {
-    Route::resource('sekolah', SekolahController::class)->except(['create','show','edit','destroy'])->whereNumber('sekolah');
-    Route::post('/kelas/duplicate', [KelasController::class, 'duplicateFromPreviousSemester'])->name('kelas.duplicate');
-    Route::resource('kriteria', KriteriaPenilaianController::class)->parameters([
-        'kriteria' => 'kriteriapenilaian',
-    ]);
-
-    Route::resource('tahun_ajaran', TahunAjaranController::class);
-    Route::resource('capaian-perkembangan', CapaianPerkembanganController::class);
-    
-    // Rute Guru
-    Route::resource('guru', GuruController::class);
-    Route::get('/guru/{id}/edit-password', [GuruController::class, 'editPassword'])->name('guru.edit-password');
-    Route::put('/guru/{id}/update-password', [GuruController::class, 'updatePassword'])->name('guru.update-password');
-    Route::put('/guru/{id}/update-user', [GuruController::class, 'updateUser'])->name('guru.update-user');
-
-    Route::resource('siswa', SiswaController::class)->except(['show'])->whereNumber('siswa');
-    Route::resource('kelas', KelasController::class)->parameters([
-        'kelas' => 'kelas'
-    ]);
-    Route::resource('karakter', KarakterController::class)->except(['show']);
-    Route::resource('pengumuman', PengumumanController::class)->except(['show'])->whereNumber('pengumuman');
-    Route::resource('indikator', IndikatorController::class);
-
+        Route::resource('anggota-kelas', AnggotaKelasController::class)->parameters([
+            'anggota-kela' => 'anggota kelas'
+        ]);
+        Route::resource('pdf', PdfController::class)->only(['show', 'index']);
     });
 
-    Route::middleware(['role:guru'])->group(function () {
-    Route::resource('kehadiran', KehadiranController::class)->only(['index','update'])->whereNumber('kehadiran');
-    
-    Route::resource('gigi', KesehatanGigiController::class)->only(['index','update'])->whereNumber('gigi');
-    Route::resource('mulut', KesehatanMulutController::class)->only(['index','update'])->whereNumber('mulut');
-    Route::resource('mata', KesehatanMataController::class)->only(['index','update'])->whereNumber('mata');
-    Route::resource('kondisi-tubuh', KondisiTubuhController::class)->only(['index','update'])->whereNumber('kondisi-tubuh');
-    Route::resource('kebersihan-siswa', KebersihanSiswaController::class)->only(['index','update'])->whereNumber('kebersihan-siswa');
-    
-    Route::get('/indikator-{kategori}', [IndikatorCapaianController::class, 'index'])->name('indikator-capaian.index');
-    Route::resource('indikator-capaian', IndikatorCapaianController::class)->except(['create','show','edit','update'])->whereNumber('indikator-capaian');
-    
-    Route::resource('nilai-karakter', NilaiKarakterController::class)->except(['create','show','edit','destroy'])->whereNumber('nilai-karakter');
-    
-    Route::get('/hasil-capaian/{slug?}', [HasilCapaianController::class, 'index'])->name('hasil-capaian.kategori');
-    Route::resource('hasil-capaian', HasilCapaianController::class)->except(['create','show','edit','destroy'])->whereNumber('hasil-capaian');
-    
-    Route::resource('kesehatan-telinga', KesehatanTelingaController::class)
-    ->except(['create','show','store','edit','destroy'])
-    ->parameters([
-        'kesehatan-telinga' => 'telinga'
-    ])
-    ->whereNumber('telinga');
+});
+
+Route::prefix('guru')->middleware(['auth', 'role:guru'])->group(function () {
 
     Route::resource('data-karakter', DataKarakterController::class)->only(['index']);
+
+    Route::get('/indikator-{kategori}', [IndikatorCapaianController::class, 'index'])->name('indikator-capaian.index');
+    Route::resource('indikator-capaian', IndikatorCapaianController::class)->except(['create','show','edit','update'])->whereNumber('indikator-capaian');
+
+    Route::resource('kondisi-tubuh', KondisiTubuhController::class)->only(['index','update'])->whereNumber('kondisi-tubuh');
+    Route::resource('kebersihan-siswa', KebersihanSiswaController::class)->only(['index','update'])->whereNumber('kebersihan-siswa');
+    Route::resource('mata', KesehatanMataController::class)->only(['index','update'])->whereNumber('mata');
+    Route::resource('kesehatan-telinga', KesehatanTelingaController::class)
+        ->except(['create','show','store','edit','destroy'])
+        ->parameters(['kesehatan-telinga' => 'telinga'])
+        ->whereNumber('telinga');
+    Route::resource('gigi', KesehatanGigiController::class)->only(['index','update'])->whereNumber('gigi');
+    Route::resource('mulut', KesehatanMulutController::class)->only(['index','update'])->whereNumber('mulut');
+
+    Route::resource('kehadiran', KehadiranController::class)->only(['index','update'])->whereNumber('kehadiran');
+    Route::resource('nilai-karakter', NilaiKarakterController::class)->except(['create','show','edit','destroy'])->whereNumber('nilai-karakter');
+
+    Route::get('/hasil-capaian/{slug?}', [HasilCapaianController::class, 'index'])->name('hasil-capaian.kategori');
+    Route::resource('hasil-capaian', HasilCapaianController::class)->except(['create','show','edit','destroy'])->whereNumber('hasil-capaian');
+
     Route::resource('catatan', CatatanController::class);
 
-    });
 });
